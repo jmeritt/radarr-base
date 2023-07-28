cc23bb6dfee902c9d0b66408da1006b574fa4a4c
jmeritt <jaime.meritt@gmail.com>
2023-07-28 07:14:30 -0400

    build update

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64 -t jmeritt/radarr . && docker push jmeritt/radarr && kubectl rollout restart deployment radarr
+docker buildx build --platform linux/amd64 -t jmeritt/radarr --no-cache . &&\ 
+docker push jmeritt/radarr &&\
+kubectl rollout restart deployment radarr
